04f3e96166fa08e819200ba34ac55e8e1c7dfdc3
staraneel <33164249+staraneelpatra@users.noreply.github.com>
2019-04-07 13:46:15 +0530

    Pushed Code

---
+++ HTMLControlViewBagTempData/HTMLControlViewBagTempData/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTMLControlViewBagTempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Validate(FormCollection f)
+        {
+            string un = f["txtun"];
+            string pwd = f["txtpwd"];
+             
+            if (un == "anil" && pwd == "anil")
+            {
+                TempData["uname"] = un;
+                return RedirectToAction("Welcome", "home");
+            }
+            else
+            {
+                ViewBag.uname = un;
+                ViewBag.msg = "Failed Authentication";
+                return View("index");
+            }
+        }
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ HTMLHelperControls/HTMLHelperControls/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTMLHelperControls.Models;
+
+namespace HTMLHelperControls.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Save(FormCollection f)
+        {
+            Storemodel sm = new Storemodel();
+            sm.Name = f["un"];
+            sm.Gender = f["r1"];
+
+            return View(sm);
+        }
+    }
+}
